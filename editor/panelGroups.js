da0c048bf527c34566cfc0d9dca9f115033faaf6
hydrogenmacro <hydrogenmacro@gmail.com>
2024-06-27 16:56:50 -0400

    implement panel group resizing

--- editor/panelGroups.js
+++ editor/panelGroups.js
@@ -5,17 +5,23 @@ const panelGroupWidths = [
 	window.innerWidth / 3
 ];
 let isAdjusting = false;
+let adjustmentState = {
+	borderToBeAdjusted: 0,
+	originalPointerPos: 0,
+	originalLeftwardPanelWidth: 0,
+	originalRightwardPanelWidth: 0
+};
 let editor = document.querySelector("#editor");
 let borderSelectTolerance = 5;
+let minEditorWidth = window.innerWidth / 5;
 const pointerStatus = {
 	down: false,
-	move: false,
 	up: false,
 	posX: 0,
 	posY: 0
 };
+let currentWindowWidth = window.innerWidth;
 editor.addEventListener("pointermove", e => {
-	pointerStatus.move = true;
 	pointerStatus.posX = e.clientX;
 	pointerStatus.posY = e.clientY;
 	update()
@@ -42,18 +48,62 @@ function update() {
 		Math.abs(panelGroupWidths[0] - pointerStatus.posX) < borderSelectTolerance,
 		Math.abs(panelGroupWidths[0] + panelGroupWidths[1] - pointerStatus.posX) < borderSelectTolerance,
 	];
-	console.log(isBetweenPanels);
-	if (isBetweenPanels[0] || isBetweenPanels[1]) {
+	if (isBetweenPanels[0] || isBetweenPanels[1] || isAdjusting) {
 		document.body.style.cursor = "col-resize";
 	} else {
 		document.body.style.cursor = "default";
 	}
 	if (pointerStatus.down) {
+		pointerStatus.down = false;
 		isAdjusting = false;
-		if (isBetweenPanels[0]) {
-			// 5 pixels away from border between left and middle panels
-			isAdjusting = true;
+		for (let i = 0; i <= 1; i++) {
+			if (isBetweenPanels[i]) {
+				// user wants to adjust panel sizes
+				isAdjusting = true;
+				adjustmentState.borderToBeAdjusted = i;
+				adjustmentState.originalPointerPos = pointerStatus.posX;
+				adjustmentState.originalLeftwardPanelWidth = panelGroupWidths[i];
+				adjustmentState.originalRightwardPanelWidth = panelGroupWidths[i + 1];
+			}
 		}
-		console.log(isAdjusting);
+	}
+	if (pointerStatus.up) {
+		pointerStatus.up = false;
+		isAdjusting = false;
+	}
+	if (isAdjusting) {
+		let adjustmentDifference = pointerStatus.posX - adjustmentState.originalPointerPos;
+		// leftward panel
+		panelGroupWidths[adjustmentState.borderToBeAdjusted] = adjustmentState.originalLeftwardPanelWidth + adjustmentDifference;
+		// rightward panel
+		panelGroupWidths[adjustmentState.borderToBeAdjusted + 1] = adjustmentState.originalRightwardPanelWidth - adjustmentDifference;
+		if (panelGroupWidths[adjustmentState.borderToBeAdjusted] < minEditorWidth) {
+			panelGroupWidths[adjustmentState.borderToBeAdjusted + 1] -= minEditorWidth - panelGroupWidths[adjustmentState.borderToBeAdjusted];
+			panelGroupWidths[adjustmentState.borderToBeAdjusted] = minEditorWidth;
+		}
+		if (panelGroupWidths[adjustmentState.borderToBeAdjusted + 1] < minEditorWidth) {
+			panelGroupWidths[adjustmentState.borderToBeAdjusted] -= minEditorWidth - panelGroupWidths[adjustmentState.borderToBeAdjusted + 1];
+			panelGroupWidths[adjustmentState.borderToBeAdjusted + 1] = minEditorWidth;
+		}
+	}
+	console.log(panelGroupWidths.reduce((acc, i) => acc + i, 0))
+}
+update()
+window.addEventListener("resize", () => {
+	let windowWidthChange = window.innerWidth - currentWindowWidth;
+	currentWindowWidth = window.innerWidth;
+	panelGroupWidths[2] += windowWidthChange;
+	// respectMinPanelGroupSize();
+})
+function respectMinPanelGroupSize() {
+	for (let i = 0; i < 3; i++) {
+		if (panelGroupWidths[i] < minEditorWidth) {
+			panelGroupWidths[i + 1] -= minEditorWidth - panelGroupWidths[i];
+			panelGroupWidths[i] = minEditorWidth;
+		}
+		if (panelGroupWidths[i + 1] < minEditorWidth) {
+			panelGroupWidths[i] -= minEditorWidth - panelGroupWidths[i + 1];
+			panelGroupWidths[i + 1] = minEditorWidth;
+		}
 	}
 }
